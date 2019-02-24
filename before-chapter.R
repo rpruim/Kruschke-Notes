d5e917b8ad5d738ffacbf4d05853b6bac42049a8
Randall Pruim <rpruim@gmail.com>
2019-02-24 11:57:00 -0500

    use before-chapter.R

---
+++ before-chapter.R
@@ -0,0 +1,19 @@
+
+knitr::opts_chunk$set(
+  fig.width = 6, fig.height = 2.5,
+  echo = TRUE
+)
+library(ggformula)
+theme_set(theme_bw(base_size = 9))
+
+library(CalvinBayes)
+library(R2jags)
+library(coda)
+library(bayesplot)
+library(mosaic)
+library(purrr)
+library(printr)
+
+
+set.seed(12345)
+options(digits = 4)
